34ccb44564b83cb4736001ba7349657b4dc511d7
Geoffrey McClinsey <gmcclins@hashbang.sh>
2017-12-27 21:03:48 -0500

    add travis

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -ev                                                                       │
+# Shellcheck the script                                                             │
+#  Ignore warning SC2029 “SSH argument is evaluated client-side”                    │
+#  Ignore warning SC2039 “In POSIX sh, 'shopt' is not supported”                    │
+#    (it is gated with `if [ -n "$BASH" ]`)                                         │
+                                                                                    │
+shellcheck -e SC2029,SC2039 install.sh 
