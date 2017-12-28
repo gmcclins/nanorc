6a16a0ddbe6f4e71dda852e60301c3d902df0618
Geoffrey McClinsey <gmcclins@hashbang.sh>
2017-12-27 21:14:38 -0500

    making tons of mistakes

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -ev                                                                       │
-# Shellcheck the script                                                             │
-#  Ignore warning SC2029 “SSH argument is evaluated client-side”                    │
-#  Ignore warning SC2039 “In POSIX sh, 'shopt' is not supported”                    │
-#    (it is gated with `if [ -n "$BASH" ]`)                                         │
-                                                                                    │
-shellcheck -e SC2029,SC2039 install.sh 
+#!/bin/sh -ev
+
+# Shellcheck the script
+#  Ignore warning SC2029 “SSH argument is evaluated client-side”
+#  Ignore warning SC2039 “In POSIX sh, 'shopt' is not supported”
+#    (it is gated with `if [ -n "$BASH" ]`)
+
+shellcheck -e SC2029,SC2039 install.sh
